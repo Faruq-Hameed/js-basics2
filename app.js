69476fe91632fc395b3d87e7aa47ec3a6d772199
Faruq-Hameed <faruqhameed1@gmail.com>
2022-10-07 12:55:52 +0100

    prime numbers assignment added

--- app.js
+++ app.js
@@ -1,107 +1,144 @@
-///////*****ASSIGNMENT 1********///////////// */
+// ///////*****ASSIGNMENT 1********///////////// */
 
-let output;
-function showNumber(limit) {
-  for (let i = 0; i <= limit; i++)
-    if (i % 2 === 0) {
-      output = i
-      console.log(output + ' EVEN')
-    }
+// let output;
+// function showNumber(limit) {
+//   for (let i = 0; i <= limit; i++)
+//     if (i % 2 === 0) {
+//       output = i
+//       console.log(output + ' EVEN')
+//     }
 
-    else {
-      console.log(i + ' ODD')
-      output = i
-    }
-  return output
-}
+//     else {
+//       console.log(i + ' ODD')
+//       output = i
+//     }
+//   return output
+// }
 
-showNumber(50) //e.g
-
-
-///////*****ASSIGNMENT 2********///////////// */
-
-// const colors = ['red',8, 'green', 'yellow', 'red', 6, 'blue', 200, 600];
-
-let totalTruthy = 0;
-
-function countTruthy(array){
-
-for (let element of array) {
-
-    // if (0 / element === 0) {
-    //     totalTruthy += 1
-        if (0 / element !== 0) continue;
-        else totalTruthy += 1
-
-}
-return totalTruthy
-
-}
-const colors = ['red',8, 'green', 'yellow', 'red', 6, 'blue', 200, 600, 0, 0];
-
-console.log(countTruthy(colors)) //eg
+// showNumber(50) //e.g
 
 
+// ///////*****ASSIGNMENT 2********///////////// */
 
-///////*****ASSIGNMENT 3********///////////// */
+// // const colors = ['red',8, 'green', 'yellow', 'red', 6, 'blue', 200, 600];
 
-const person = {
-    name: 'Faruq',
-    age: 27,
-    school: 'UI',
-    matriculationYear: 2016,
-    course: 'Agric-Econs',
-    courseYears: 5,
-    graduationYear: 2021,
-    friend: '',
-}
+// let totalTruthy = 0;
 
-function showProperties(object) {
-    for (key in object) {
-        if (0 / object[key] === 0) continue;
-        else if (!object[key]) continue; /* Added this to take care of falsy values */
-        console.log('The ' + key + ' is', object[key]);
-    }
+// function countTruthy(array){
 
-}
-showProperties(person) //eg
+// for (let element of array) {
+
+//     if (0 / element === 0) {
+//         totalTruthy += 1
+//     }
+//         if (0 / element !== 0) continue;
+//         else totalTruthy += 1
+
+// }
+// return totalTruthy
+
+// }
+// const colors = ['red',8, 'green', 'yellow', 'red', 6, 'blue', 200, 600, 0, 0];
+
+// console.log(countTruthy(colors)) //eg
 
 
 
-///////*****ASSIGNMENT 4********///////////// */
+// ///////*****ASSIGNMENT 3********///////////// */
 
-let result = 0;
-function sum(limit) {
-  for (let i = 1; i < limit; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      result += i;
+// const person = {
+//     name: 'Faruq',
+//     age: 27,
+//     school: 'UI',
+//     matriculationYear: 2016,
+//     course: 'Agric-Econs',
+//     courseYears: 5,
+//     graduationYear: 2021,
+//     friend: '',
+// }
 
-    }
-  }
-  console.log(result);
+// function showProperties(object) {
+//     for (key in object) {
+//         if (0 / object[key] === 0) continue;
+//         else if (!object[key]) continue; /* Added this to take care of falsy values */
+//         console.log('The ' + key + ' is', object[key]);
+//     }
 
-  return result;
-}
-
-sum(10) //eg
+// }
+// showProperties(person) //eg
 
 
 
-///////*****ASSIGNMENT 5********///////////// */
+// ///////*****ASSIGNMENT 4********///////////// */
 
-function showPrimes(n) {
-    jumpHere: for (let i = 2; i <= n; i++) {
+// let result = 0;
+// function sum(limit) {
+//   for (let i = 1; i <= limit; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       result += i;
+
+//     }
+//   }
+//   console.log(result);
+
+//   return result;
+// }
+
+// sum(10) //eg
+
+
+// let result = 0;
+// function sum(limit) {
+//   for (let i = 1; i < limit; i++) {
+//     if (i % 3 === 0 || i % 5 === 0) {
+//       result += i;
+
+//     }
+    
+//   }
   
-      for (let j = 2; j < i; j++) {
-        if (i % j == 0) {continue jumpHere;} // Label to jump
-    }
-    console.log(i); // a prime
-    }
-  }
+//   console.log(result);
+// }
 
-  showPrimes(24) //eg
+// sum(10) //eg
 
 
 
+// ///////*****ASSIGNMENT 5********///////////// */
+
+// function showPrimes(n) {
+
+//     jumpHere: for (let i = 2; i <= n; i++) {
+  
+//       for (let j = 2; j < i; j++) {
+//         if (i % j == 0) {continue jumpHere;} // Label to jump
+        
+//     }
+//     console.log(i);// a prime
+//     }
+
+// }
+
+//   showPrimes(10) //eg
+
+
+  
+  // function isPrime(n) {
+  //   for (let i = 2; i < n; i++) {
+  //     if ( n % i == 0) return false;
+  //   }
+  //   return true;
+  // }
+
+  // function showPrimes(n) {
+
+  //   for (let i = 2; i < n; i++) {
+  //     if (!isPrime(i)) continue;
+  
+  //     console.log(i);  // a prime
+  //   }
+  // }
+
+  // showPrimes(10)
 
 
